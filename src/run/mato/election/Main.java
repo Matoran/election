07859d64a8488d3502443191ddf5516ef4a928b5
matoran <marco.rodrigues-lopes@etu.hesge.ch>
2017-10-11 11:32:47 +0200

    edit neighbors, check params

--- src/run/mato/election/Main.java
+++ src/run/mato/election/Main.java
@@ -3,6 +3,13 @@ package run.mato.election;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        if(args.length < 3){
+            System.out.println("prog num_port voisin-x.txt INIT|WAIT");
+            System.exit(1);
+        }
+        int port = Integer.valueOf(args[1]);
+        String file = args[2];
+        boolean candidat = args[3].equals("INIT");
+        Node node = new Node(port, file, candidat);
     }
 }
--- src/run/mato/election/Node.java
+++ src/run/mato/election/Node.java
@@ -1,24 +1,50 @@
 package run.mato.election;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Node {
-    private int condition;
-    private int id;
-    private boolean candidat;
-    private ArrayList<Node> tried;
-    private Node owner;
+    private ArrayList<Integer> neighbors;
+    private HashMap<Integer, Boolean> tried;
+    private int owner;
 
-    public Node(int id, boolean candidat) {
-        this.id = id;
-        this.candidat = candidat;
+    public Node(int id, String filename, boolean candidat) {
         Random rand = new Random();
-        condition = rand.nextInt(10);
-        loadFile();
+        int condition = rand.nextInt(10);
+        loadFile(filename);
+        int index = 0;
+        if(candidat){
+            owner = id;
+            try {
+                ServerSocket serverSocket = new ServerSocket(id);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //neighbors.get(0);
+        }
     }
 
-    private void loadFile() {
+    private void loadFile(String filename) {
+        File file = new File(filename);
+        try {
+            Scanner sc = new Scanner(file);
 
+            while (sc.hasNextLine()) {
+                int i = sc.nextInt();
+                neighbors.add(i);
+                tried.put(i, false);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
